7a347e231123789923ae25aab489b1807911d4db
Sachin M G <92221521+git-sachin@users.noreply.github.com>
2025-05-26 14:20:38 +0530

    Add files via upload

---
+++ Retail_analytics_tables.sql
@@ -0,0 +1,8 @@
+SELECT*FROM `sales_transaction-1714027462 (1)`;
+DESC `sales_transaction-1714027462 (1)`;
+
+SELECT*FROM `product_inventory-1-1714027438`;
+DESC `product_inventory-1-1714027438`;
+
+SELECT*FROM `customer_profiles-1-1714027410`;
+DESC `customer_profiles-1-1714027410`;
